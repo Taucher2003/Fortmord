ed44dc96c26a0bdddf25f7afad1086171955a972
Niklas van Schrick <mc.taucher2003@gmail.com>
2022-11-26 23:18:40 +0100

    prog

--- src/main/java/club/devcord/gamejam/devmarkt_working_group/fortmord/level/InventoryLevelCalculator.java
+++ src/main/java/club/devcord/gamejam/devmarkt_working_group/fortmord/level/InventoryLevelCalculator.java
@@ -26,37 +26,37 @@ import java.util.stream.Stream;
 
 @Singleton
 public class InventoryLevelCalculator implements LevelCalculator.Inner {
+    private final Server server;
+    private final Map<Material, Double> recipeCache = new EnumMap<>(Material.class);
 
-    private static final Map<Material, Double> BASE_LEVELS = new EnumMap<>(Material.class);
+    public InventoryLevelCalculator(Server server) {
+        this.server = server;
 
-    private static void put(Double level, Material... materials) {
-        for (var material : materials) {
-            BASE_LEVELS.put(material, level);
-        }
-    }
-
-    static {
         var index = 0D;
         put(++index, Material.ACACIA_WOOD, Material.BIRCH_WOOD, Material.DARK_OAK_WOOD, Material.JUNGLE_WOOD, Material.MANGROVE_WOOD, Material.OAK_WOOD, Material.SPRUCE_WOOD);
         put(++index, Material.COBBLESTONE, Material.COBBLED_DEEPSLATE);
         put(++index, Material.LEATHER);
         put(++index, Material.IRON_ORE, Material.DEEPSLATE_IRON_ORE, Material.RAW_IRON, Material.GOLD_ORE, Material.DEEPSLATE_GOLD_ORE, Material.RAW_GOLD);
         put(++index, Material.DIAMOND_ORE, Material.DEEPSLATE_DIAMOND_ORE, Material.EMERALD_ORE, Material.DEEPSLATE_EMERALD_ORE);
+        put(++index, Material.ACACIA_WOOD, Material.BIRCH_WOOD, Material.DARK_OAK_WOOD, Material.JUNGLE_WOOD, Material.MANGROVE_WOOD, Material.OAK_WOOD, Material.SPRUCE_WOOD);
+        put(++index, Material.COBBLESTONE, Material.COBBLED_DEEPSLATE);
+        put(++index, Material.LEATHER);
+        put(++index, Material.IRON_ORE, Material.DEEPSLATE_IRON_ORE, Material.GOLD_ORE, Material.DEEPSLATE_GOLD_ORE);
+        put(++index, Material.DIAMOND_ORE, Material.DEEPSLATE_DIAMOND_ORE, Material.EMERALD_ORE, Material.DEEPSLATE_EMERALD_ORE);
+        put(++index, Material.ANCIENT_DEBRIS);
     }
-
-    private final Server server;
-    private final Map<Material, Double> recipeCache = new EnumMap<>(Material.class);
-
-    public InventoryLevelCalculator(Server server) {
-        this.server = server;
+    private void put(Double level, Material... materials) {
+        for (var material : materials) {
+            recipeCache.put(material, level);
+        }
     }
 
     @Override
     public double calculate(Player player) {
         return Arrays.stream(player.getInventory().getContents())
                 .filter(Objects::nonNull)
-                .map(this::calculateItem)
-                .reduce(0D, Double::sum);
+                .mapToDouble(this::calculateItem)
+                .sum();
     }
 
     @Override
@@ -65,9 +65,7 @@ public class InventoryLevelCalculator implements LevelCalculator.Inner {
     }
 
     private double calculateItem(ItemStack item) {
-        var result = IntStream.range(0, item.getAmount())
-                .mapToDouble(ignored -> calculateBase(item.getType(), EnumSet.noneOf(Material.class)))
-                .sum();
+        var result = calculateBase(item.getType()) * item.getAmount();
 
         result += item.getEnchantments().values().stream().mapToDouble(level -> level * 0.5).reduce(0, Double::sum);
 
@@ -110,34 +108,31 @@ public class InventoryLevelCalculator implements LevelCalculator.Inner {
                 .sum();
     }
 
-    private Stream<Material> ingredientsForRecipe(Recipe recipe){
+    private Stream<ItemStack> ingredientsForRecipe(Recipe recipe){
         if(recipe instanceof CookingRecipe<?> cookingRecipe) {
-            return itemstackToMaterialList(cookingRecipe.getInputChoice().getItemStack());
+            server.broadcastMessage(cookingRecipe.getInputChoice().getItemStack().toString());
+            return Stream.of(cookingRecipe.getInputChoice().getItemStack());
         }
         if(recipe instanceof MerchantRecipe merchantRecipe) {
             return merchantRecipe.getIngredients()
-                    .stream()
-                    .flatMap(this::itemstackToMaterialList);
+                    .stream();
         }
         if(recipe instanceof SmithingRecipe smithingRecipe) {
-            return Stream.of(smithingRecipe.getBase().getItemStack(), smithingRecipe.getAddition().getItemStack())
-                    .flatMap(this::itemstackToMaterialList);
+            return Stream.of(smithingRecipe.getBase().getItemStack(), smithingRecipe.getAddition().getItemStack());
         }
         if(recipe instanceof StonecuttingRecipe stonecuttingRecipe) {
-            return itemstackToMaterialList(stonecuttingRecipe.getInputChoice().getItemStack());
+            return Stream.of(stonecuttingRecipe.getInputChoice().getItemStack());
         }
         if(recipe instanceof ShapedRecipe shapedRecipe) {
             return shapedRecipe.getChoiceMap().values()
                     .stream()
                     .filter(Objects::nonNull)
-                    .map(RecipeChoice::getItemStack)
-                    .flatMap(this::itemstackToMaterialList);
+                    .map(RecipeChoice::getItemStack);
         }
         if(recipe instanceof ShapelessRecipe shapelessRecipe) {
             return shapelessRecipe.getChoiceList()
                     .stream()
-                    .map(RecipeChoice::getItemStack)
-                    .flatMap(this::itemstackToMaterialList);
+                    .map(RecipeChoice::getItemStack);
         }
 
         return Stream.empty();
