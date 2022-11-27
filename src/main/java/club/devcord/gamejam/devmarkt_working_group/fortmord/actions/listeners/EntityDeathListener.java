52af5a3f0522749f02699ea9cb51da38868fac23
Nick Hensel <nickhensel25@icloud.com>
2022-11-27 15:34:12 +0100

    prog

--- src/main/java/club/devcord/gamejam/devmarkt_working_group/fortmord/actions/listeners/EntityDeathListener.java
+++ src/main/java/club/devcord/gamejam/devmarkt_working_group/fortmord/actions/listeners/EntityDeathListener.java
@@ -3,12 +3,14 @@ package club.devcord.gamejam.devmarkt_working_group.fortmord.actions.listeners;
 import jakarta.inject.Singleton;
 import org.bukkit.World;
 import org.bukkit.entity.Blaze;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 
 @Singleton
 public class EntityDeathListener implements Listener {
 
+    @EventHandler
     public void onEntityDeath(EntityDeathEvent event) {
         if (event.getEntity().getWorld().getEnvironment() == World.Environment.NORMAL) {
             if (event.getEntity() instanceof Blaze blaze)
