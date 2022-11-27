f59daa6e4e1100e21d7866d229fd56c123e28213
Niklas van Schrick <mc.taucher2003@gmail.com>
2022-11-27 02:47:33 +0100

    prog

--- src/main/java/club/devcord/gamejam/devmarkt_working_group/fortmord/actions/effects/HealthLimiterAction.java
+++ src/main/java/club/devcord/gamejam/devmarkt_working_group/fortmord/actions/effects/HealthLimiterAction.java
@@ -4,25 +4,27 @@ import club.devcord.gamejam.devmarkt_working_group.fortmord.FortMord;
 import club.devcord.gamejam.devmarkt_working_group.fortmord.actions.AbstractLevelAction;
 import club.devcord.gamejam.devmarkt_working_group.fortmord.actions.LevelAction;
 import io.micronaut.context.annotation.Parameter;
+import org.bukkit.Server;
 import org.bukkit.entity.Player;
 
 import javax.annotation.PostConstruct;
-import java.util.concurrent.ThreadLocalRandom;
 
-@LevelAction(value = 50, levelBound = true)
-public class DamageEffectAction extends AbstractLevelAction {
-    public DamageEffectAction(@Parameter Player player, FortMord fortMord, @Parameter int level) {
+@LevelAction(value = 25, levelBound = false)
+public class HealthLimiterAction extends AbstractLevelAction {
+    private final Server server;
+
+    public HealthLimiterAction(@Parameter Player player, FortMord fortMord, Server server, @Parameter int level) {
         super(player, level, fortMord);
+        this.server = server;
     }
 
     @PostConstruct
     void start() {
-        runTaskTimer(this::apply, 5, () -> random(descLevel(), 257) * 10L);
+        runTaskTimer(this::apply, 5, () -> random(40, 200));
     }
 
     private void apply() {
-        if(ThreadLocalRandom.current().nextBoolean() && player().getHealth() > 10) {
-            player().damage(ThreadLocalRandom.current().nextDouble(2));
-        }
+        player().setMaxHealth(random(14, 18));
+        server.getScheduler().runTaskLater(fortMord(), player()::resetMaxHealth, random(40, 60));
     }
 }
